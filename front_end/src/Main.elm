bc933f281d257508a0dc2577202f6ed9291cbebf
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-03-23 16:48:01 +1100

    Get an initial survey results list displaying

--- front_end/src/Main.elm
+++ front_end/src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, a, article, div, h1, h2, img, main_, p, section, span, text)
+import Html.Attributes exposing (alt, attribute, class, href, src)
 
 
 ---- MODEL ----
@@ -35,8 +35,47 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Your Elm App is working!" ]
+    main_ []
+        [ section [ attribute "data-name" "survey-results", class "mw7 center" ]
+            [ h1 [ class "tc ttu f1 f-5-ns mv4" ]
+                [ text "Survey Results" ]
+            , surveyResult "Simple Survey" "6" "5" "83%"
+            , surveyResult "Acme Engagement Survey" "271" "271" "100%"
+            ]
+        ]
+
+
+surveyResult title numParticipants numResponses responseRate =
+    article [ class "ba b--black-10 mt2 grow grow:focus grow:active" ]
+        [ a
+            [ class "db pv1 ph0 no-underline black", href "#" ]
+            [ h1 [ class "tc f2 f1-ns mt0 mb2" ]
+                [ text title ]
+            , div
+                [ class "flex flex-column flex-row-ns justify-around ph4 ph0-ns" ]
+                [ div [ class "w-50-ns" ]
+                    [ div [ class "flex justify-between" ]
+                        [ div [ class "f2 f1-ns" ]
+                            [ text "Participants" ]
+                        , div [ class "f2 f1-ns" ]
+                            [ text numParticipants ]
+                        ]
+                    , div [ class "flex justify-between" ]
+                        [ div [ class "f2 f1-ns" ]
+                            [ text "Responses" ]
+                        , div [ class "f2 f1-ns" ]
+                            [ text numResponses ]
+                        ]
+                    ]
+                , div
+                    [ class "tc flex flex-column-ns mt2 mt0-ns justify-between" ]
+                    [ div [ class "f2 ttu" ]
+                        [ text "Response Rate" ]
+                    , div [ class "f2 f1-ns" ]
+                        [ text responseRate ]
+                    ]
+                ]
+            ]
         ]
 
 
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,7 +1,8 @@
-import './main.css';
-import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
+// import './main.css';
+import "tachyons";
+import { Main } from "./Main.elm";
+// import registerServiceWorker from './registerServiceWorker';
 
-Main.embed(document.getElementById('root'));
+Main.embed(document.getElementById("root"));
 
-registerServiceWorker();
+// registerServiceWorker();
