178592d2dc99e8df3500b40572994b08f9b8af69
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-03-25 16:16:20 +1100

    remove PWA files

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,8 +1,5 @@
 import "tachyons";
 import "./main.css";
 import { Main } from "./Main.elm";
-// import registerServiceWorker from './registerServiceWorker';
 
 Main.embed(document.getElementById("root"));
-
-// registerServiceWorker();
