756418f4de53e82ee91fef2bf228e248bbd8f78b
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-03-27 14:40:45 +1100

    Add CORS config for Elm apps in dev mode

--- back_end/config/dev.exs
+++ back_end/config/dev.exs
@@ -1,3 +1,9 @@
 use Mix.Config
 
 config :back_end, cowboy_port: 4000
+
+# Allow CORS requests from development Elm app
+config :cors_plug,
+  origin: ["http://localhost:3000"],
+  max_age: 86400,
+  methods: ["GET"]
