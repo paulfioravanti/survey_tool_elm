0ee6e20d08728ec46049bf000f9ae18e7f753728
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-03-31 23:54:01 +1100

    Add verified example for survey result decoder

--- front_end/src/SurveyResult/Decoder.elm
+++ front_end/src/SurveyResult/Decoder.elm
@@ -5,6 +5,36 @@ import Json.Decode.Extra exposing ((|:))
 import SurveyResult.Model exposing (SurveyResult)
 
 
+{-| Decodes a JSON survey result.
+
+    import Json.Decode
+    import SurveyResult.Model exposing (SurveyResult)
+
+    json : String
+    json =
+        """
+        {
+          "url": "/survey_results/1.json",
+          "submitted_response_count": 5,
+          "response_rate": 0.8333333333333334,
+          "participant_count": 6,
+          "name": "Simple Survey"
+        }
+        """
+
+    surveyResult : SurveyResult
+    surveyResult =
+        SurveyResult
+            "Simple Survey"
+            6
+            0.8333333333333334
+            5
+            "/survey_results/1.json"
+
+    Json.Decode.decodeString decoder json
+    --> Ok surveyResult
+
+-}
 decoder : Decode.Decoder SurveyResult
 decoder =
     Decode.succeed
