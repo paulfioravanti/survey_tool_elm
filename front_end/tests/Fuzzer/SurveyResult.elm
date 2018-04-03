e81ca262d28230f024ec1174316cfb2f8a772dc8
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-04-03 22:38:46 +1000

    Fix fuzzer to give a proper id to survey results

--- front_end/tests/Fuzzer/SurveyResult.elm
+++ front_end/tests/Fuzzer/SurveyResult.elm
@@ -1,13 +1,19 @@
 module Fuzzer.SurveyResult exposing (fuzzer)
 
-import Fuzz exposing (Fuzzer, float, int, string)
+import Fuzz exposing (Fuzzer, constant, float, int, string)
+import Fuzzer.SurveyResultDetailId as SurveyResultDetailId
 import SurveyResult.Model exposing (SurveyResult)
 
 
 fuzzer : Fuzzer SurveyResult
 fuzzer =
-    Fuzz.map SurveyResult string
-        |> Fuzz.andMap int
-        |> Fuzz.andMap float
-        |> Fuzz.andMap int
-        |> Fuzz.andMap string
+    SurveyResultDetailId.fuzzer
+        |> Fuzz.andThen
+            (\id ->
+                Fuzz.map SurveyResult string
+                    |> Fuzz.andMap int
+                    |> Fuzz.andMap float
+                    |> Fuzz.andMap int
+                    |> Fuzz.andMap
+                        (constant ("/survey_results/" ++ id ++ ".json"))
+            )
