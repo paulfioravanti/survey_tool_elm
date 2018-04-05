afcfcceb02eb65f66ec4dbec3745dd7f84b1f033
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-04-05 17:11:54 +1000

    Write tests for survey result detail records to bring suite back up to full coverage

---
+++ front_end/tests/SurveyResultDetail/UpdateTest.elm
@@ -0,0 +1,101 @@
+module SurveyResultDetail.UpdateTest exposing (suite)
+
+import Expect
+import Fuzzer.Config as Config
+import Fuzzer.SurveyResult as SurveyResult
+import Helpers
+import Http exposing (Error(NetworkError))
+import Model exposing (Model)
+import Msg exposing (Msg(SurveyResultMsg))
+import RemoteData
+    exposing
+        ( RemoteData
+            ( Failure
+            , NotRequested
+            , Requesting
+            , Success
+            )
+        )
+import Result exposing (Result)
+import Routing.Route exposing (Route(SurveyResultDetailRoute))
+import SurveyResult.Msg exposing (Msg(FetchSurveyResult))
+import Test exposing (Test, describe, fuzz2)
+import Update
+
+
+suite : Test
+suite =
+    let
+        surveyResult =
+            SurveyResult.fuzzer
+
+        config =
+            Config.fuzzer
+    in
+        describe "update when SurveyResultMsg msg sent"
+            [ fuzz2
+                config
+                surveyResult
+                """
+                model surveyResultDetail is updated with a Failure when
+                FetchSurveyResult request fails
+                """
+              <|
+                \config surveyResultDetail ->
+                    let
+                        id =
+                            surveyResultDetail.url
+                                |> Helpers.extractSurveyResultDetailId
+
+                        model =
+                            Model
+                                config
+                                (SurveyResultDetailRoute id)
+                                Requesting
+                                NotRequested
+
+                        msg =
+                            SurveyResultMsg
+                                (FetchSurveyResult (Err NetworkError))
+                    in
+                        model
+                            |> Update.update msg
+                            |> Tuple.first
+                            |> Expect.equal
+                                { model
+                                    | surveyResultDetail = Failure NetworkError
+                                }
+            , fuzz2
+                config
+                surveyResult
+                """
+                model surveyResultDetail is updated with a Success when
+                FetchSurveyResult request succeeds
+                """
+              <|
+                \config surveyResultDetail ->
+                    let
+                        id =
+                            surveyResultDetail.url
+                                |> Helpers.extractSurveyResultDetailId
+
+                        model =
+                            Model
+                                config
+                                (SurveyResultDetailRoute id)
+                                NotRequested
+                                Requesting
+
+                        msg =
+                            SurveyResultMsg
+                                (FetchSurveyResult (Ok surveyResultDetail))
+                    in
+                        model
+                            |> Update.update msg
+                            |> Tuple.first
+                            |> Expect.equal
+                                { model
+                                    | surveyResultDetail =
+                                        Success surveyResultDetail
+                                }
+            ]
