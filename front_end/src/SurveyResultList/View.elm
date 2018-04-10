5fc952a4f6fe8f618406c6087dcc16890576ce93
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-04-10 16:00:33 +1000

    View refactor

--- front_end/src/SurveyResultList/View.elm
+++ front_end/src/SurveyResultList/View.elm
@@ -3,20 +3,39 @@ module SurveyResultList.View exposing (view)
 import Html.Styled exposing (Html, div, h1, h4, img, main_, section, span, text)
 import Html.Styled.Attributes exposing (alt, attribute, class, src)
 import SurveyResult.View
+import SurveyResult.Model exposing (SurveyResult)
 import SurveyResultList.Model exposing (SurveyResultList)
 
 
 view : (String -> msg) -> SurveyResultList -> Html msg
 view msg { surveyResults } =
-    main_ []
-        [ section [ attribute "data-name" "survey-results", class "center mw7" ]
-            (div [ class "flex justify-around" ]
-                [ heading ]
-                :: (surveyResults
-                        |> List.map (SurveyResult.View.view msg)
-                   )
-            )
-        ]
+    let
+        classes =
+            [ "center"
+            , "mw7"
+            ]
+                |> String.join " "
+    in
+        main_ []
+            [ section [ class classes ]
+                (surveyResultList msg surveyResults)
+            ]
+
+
+surveyResultList : (String -> msg) -> List SurveyResult -> List (Html msg)
+surveyResultList msg surveyResults =
+    let
+        classes =
+            [ "flex"
+            , "justify-around"
+            ]
+                |> String.join " "
+    in
+        div [ class classes ]
+            [ heading ]
+            :: (surveyResults
+                    |> List.map (SurveyResult.View.view msg)
+               )
 
 
 heading : Html msg
