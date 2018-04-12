988f487dd13c1d0a8b134003b7b2960f7da7a1a9
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-04-12 16:04:47 +1000

    More test refactoring

--- front_end/tests/SurveyResultList/UpdateTest.elm
+++ front_end/tests/SurveyResultList/UpdateTest.elm
@@ -1,6 +1,8 @@
 module SurveyResultList.UpdateTest exposing (suite)
 
+import Config exposing (Config)
 import Expect
+import Fuzz exposing (Fuzzer)
 import Fuzzer.Config as Config
 import Fuzzer.SurveyResultList as SurveyResultList
 import Http exposing (Error(NetworkError))
@@ -25,47 +27,53 @@ import Update
 suite : Test
 suite =
     let
-        surveyResultList =
-            SurveyResultList.fuzzer
-
         config =
             Config.fuzzer
     in
-        describe "update when SurveyResultListMsg msg sent"
-            [ fuzz
-                config
-                """
-                model surveyResultList is updated with a Failure when
-                FetchSurveyResultList request fails
-                """
-              <|
-                \config ->
-                    let
-                        model =
-                            Model
-                                config
-                                ListSurveyResultsRoute
-                                NotRequested
-                                Requesting
+        describe "update"
+            [ fetchSurveyResultListFailureTest config
+            , fetchSurveyResultListSuccessTest config
+            ]
 
-                        msg =
-                            SurveyResultListMsg
-                                (FetchSurveyResultList (Err NetworkError))
-                    in
-                        model
-                            |> Update.update msg
-                            |> Tuple.first
-                            |> Expect.equal
-                                { model
-                                    | surveyResultList = Failure NetworkError
-                                }
-            , fuzz2
+
+fetchSurveyResultListFailureTest : Fuzzer Config -> Test
+fetchSurveyResultListFailureTest config =
+    describe "when SurveyResultListMsg FetchSurveyResultList request fails"
+        [ fuzz config "model surveyResultList is updated with a Failure" <|
+            \config ->
+                let
+                    model =
+                        Model
+                            config
+                            ListSurveyResultsRoute
+                            NotRequested
+                            Requesting
+
+                    msg =
+                        SurveyResultListMsg
+                            (FetchSurveyResultList (Err NetworkError))
+                in
+                    model
+                        |> Update.update msg
+                        |> Tuple.first
+                        |> Expect.equal
+                            { model
+                                | surveyResultList = Failure NetworkError
+                            }
+        ]
+
+
+fetchSurveyResultListSuccessTest : Fuzzer Config -> Test
+fetchSurveyResultListSuccessTest config =
+    let
+        surveyResultList =
+            SurveyResultList.fuzzer
+    in
+        describe "SurveyResultListMsg FetchSurveyResultList request succeeds"
+            [ fuzz2
                 config
                 surveyResultList
-                """
-                model surveyResultList is updated with a Success when
-                FetchSurveyResultList request succeeds
-                """
+                "model surveyResultList is updated with a Success"
               <|
                 \config surveyResultList ->
                     let
