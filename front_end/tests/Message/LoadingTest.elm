e308b67d2275de92bf2c0ee51cae967e2e6ca25a
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-04-13 15:53:41 +1000

    Refactor loading test

--- front_end/tests/Message/LoadingTest.elm
+++ front_end/tests/Message/LoadingTest.elm
@@ -1,6 +1,8 @@
 module Message.LoadingTest exposing (suite)
 
+import Config exposing (Config)
 import Expect
+import Fuzz exposing (Fuzzer)
 import Fuzzer.Config as Config
 import Html.Attributes as Attributes
 import Html.Styled
@@ -16,7 +18,7 @@ import Routing.Route
         )
 import Test exposing (Test, describe, fuzz)
 import Test.Html.Query as Query
-import Test.Html.Selector exposing (attribute, tag)
+import Test.Html.Selector as Selector exposing (Selector, tag)
 
 
 suite : Test
@@ -26,49 +28,54 @@ suite =
             Config.fuzzer
 
         loadingMessage =
-            attribute
-                (Attributes.attribute
-                    "data-name"
-                    "loading-message"
-                )
+            Selector.attribute
+                (Attributes.attribute "data-name" "loading-message")
     in
         describe "view"
-            [ describe
-                "when data has been requested on the SurveyResultsList page"
-                [ fuzz config "displays a loading message" <|
-                    \config ->
-                        let
-                            model =
-                                Model
-                                    config
-                                    ListSurveyResultsRoute
-                                    NotRequested
-                                    Requesting
-                        in
-                            model
-                                |> Router.route
-                                |> Html.Styled.toUnstyled
-                                |> Query.fromHtml
-                                |> Query.find [ tag "section" ]
-                                |> Query.has [ loadingMessage ]
-                ]
-            , describe
-                "when data has been requested on a SurveyResultDetail page"
-                [ fuzz config "displays a loading message" <|
-                    \config ->
-                        let
-                            model =
-                                Model
-                                    config
-                                    (SurveyResultDetailRoute "10")
-                                    Requesting
-                                    NotRequested
-                        in
-                            model
-                                |> Router.route
-                                |> Html.Styled.toUnstyled
-                                |> Query.fromHtml
-                                |> Query.find [ tag "section" ]
-                                |> Query.has [ loadingMessage ]
-                ]
+            [ surveyResultsListPageTest config loadingMessage
+            , surveyResultDetailPageTest config loadingMessage
             ]
+
+
+surveyResultsListPageTest : Fuzzer Config -> Selector -> Test
+surveyResultsListPageTest config loadingMessage =
+    describe "when data has been requested on the SurveyResultsList page"
+        [ fuzz config "displays a loading message" <|
+            \config ->
+                let
+                    model =
+                        Model
+                            config
+                            ListSurveyResultsRoute
+                            NotRequested
+                            Requesting
+                in
+                    model
+                        |> Router.route
+                        |> Html.Styled.toUnstyled
+                        |> Query.fromHtml
+                        |> Query.find [ tag "section" ]
+                        |> Query.has [ loadingMessage ]
+        ]
+
+
+surveyResultDetailPageTest : Fuzzer Config -> Selector -> Test
+surveyResultDetailPageTest config loadingMessage =
+    describe "when data has been requested on a SurveyResultDetail page"
+        [ fuzz config "displays a loading message" <|
+            \config ->
+                let
+                    model =
+                        Model
+                            config
+                            (SurveyResultDetailRoute "10")
+                            Requesting
+                            NotRequested
+                in
+                    model
+                        |> Router.route
+                        |> Html.Styled.toUnstyled
+                        |> Query.fromHtml
+                        |> Query.find [ tag "section" ]
+                        |> Query.has [ loadingMessage ]
+        ]
