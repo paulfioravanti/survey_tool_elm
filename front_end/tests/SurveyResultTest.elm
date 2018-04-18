06d4d24f5b8bcac890f41604b0ab646a3399789b
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-04-18 21:25:22 +1000

    Extract SurveyResult decoder test into top level survey result test

--- front_end/tests/SurveyResultTest.elm
+++ front_end/tests/SurveyResultTest.elm
@@ -1,24 +1,30 @@
-module SurveyResult.DecoderTest exposing (suite)
+module SurveyResultTest exposing (suite)
 
 import Expect
 import Fuzzer.SurveyResult as SurveyResult
 import Json.Decode as Decode
-import SurveyResult.Decoder as Decoder
+import SurveyResult
 import SurveyResult.Encoder as Encoder
 import Test exposing (Test, describe, fuzz)
 
 
 suite : Test
 suite =
+    describe "SurveyResult"
+        [ decoderTests () ]
+
+
+decoderTests : () -> Test
+decoderTests () =
     let
         surveyResult =
             SurveyResult.fuzzer
     in
-        describe "SurveyResult.Decoder"
+        describe "decoder"
             [ fuzz surveyResult "decoder maps to a SurveyResult" <|
                 \surveyResult ->
                     surveyResult
                         |> Encoder.encoder
-                        |> Decode.decodeValue Decoder.decoder
+                        |> Decode.decodeValue SurveyResult.decoder
                         |> Expect.equal (Ok surveyResult)
             ]
