97d7e0b5390d0b064894e16bd3cdaddbf5ac34b2
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-04-22 22:49:05 +1000

    Minor refactor

--- front_end/src/Router/Update.elm
+++ front_end/src/Router/Update.elm
@@ -5,8 +5,8 @@ module Router.Update exposing (update)
 
 import I18Next exposing (Translations)
 import Navigation
-import Router.Msg exposing (Msg(Blur, ChangeLocation, OnLocationChange))
 import Route exposing (Route)
+import Router.Msg exposing (Msg(Blur, ChangeLocation, OnLocationChange))
 import Router.Utils as Utils
 import Task
 import Window
