1058b6d618d7892db0c41caee2f5a0db018ec1cd
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-05-01 09:36:11 +1000

    Add test for showing flags in language dropdown for current language

---
+++ front_end/tests/Locale/DropdownTest.elm
@@ -0,0 +1,133 @@
+module Locale.DropdownTest exposing (suite)
+
+import Config exposing (Config)
+import Expect
+import Fuzz exposing (Fuzzer)
+import Fuzzer.Config as Config
+import Fuzzer.Navigation.Location as Location
+import Fuzzer.SurveyResultList as SurveyResultList
+import I18Next
+import Html.Attributes as Attributes
+import Html.Styled
+import Locale.Model exposing (Language(En, It, Ja), Locale)
+import Main
+import Model exposing (Model)
+import Navigation exposing (Location)
+import RemoteData exposing (RemoteData(NotRequested, Success))
+import Route exposing (Route(ListSurveyResultsRoute))
+import SurveyResultList exposing (SurveyResultList)
+import Test exposing (Test, describe, fuzz3)
+import Test.Html.Query as Query
+import Test.Html.Selector as Selector exposing (attribute, class, tag)
+
+
+suite : Test
+suite =
+    let
+        config =
+            Config.fuzzer
+
+        location =
+            Location.fuzzer
+
+        surveyResultList =
+            SurveyResultList.fuzzer
+    in
+        describe "language dropdown menu"
+            [ currentLanguageFlagDisplayTests config location surveyResultList ]
+
+
+currentLanguageFlagDisplayTests :
+    Fuzzer Config
+    -> Fuzzer Location
+    -> Fuzzer SurveyResultList
+    -> Test
+currentLanguageFlagDisplayTests config location surveyResultList =
+    let
+        dropdownCurrentSelection =
+            Selector.attribute
+                (Attributes.attribute
+                    "data-name"
+                    "locale-dropdown-current-selection"
+                )
+    in
+        describe "flag representations for language"
+            [ fuzz3
+                config
+                location
+                surveyResultList
+                "shows the Australian flag for the English language"
+                (\config location surveyResultList ->
+                    let
+                        locale =
+                            (Locale En False I18Next.initialTranslations)
+
+                        model =
+                            Model
+                                config
+                                locale
+                                location
+                                ListSurveyResultsRoute
+                                NotRequested
+                                (Success surveyResultList)
+                    in
+                        model
+                            |> Main.view
+                            |> Query.fromHtml
+                            |> Query.find [ dropdownCurrentSelection ]
+                            |> Query.children [ class "flag-icon-au" ]
+                            |> Query.count (Expect.equal 1)
+                )
+            , fuzz3
+                config
+                location
+                surveyResultList
+                "shows the Italian flag for the Italian language"
+                (\config location surveyResultList ->
+                    let
+                        locale =
+                            (Locale It False I18Next.initialTranslations)
+
+                        model =
+                            Model
+                                config
+                                locale
+                                location
+                                ListSurveyResultsRoute
+                                NotRequested
+                                (Success surveyResultList)
+                    in
+                        model
+                            |> Main.view
+                            |> Query.fromHtml
+                            |> Query.find [ dropdownCurrentSelection ]
+                            |> Query.children [ class "flag-icon-it" ]
+                            |> Query.count (Expect.equal 1)
+                )
+            , fuzz3
+                config
+                location
+                surveyResultList
+                "shows the Japanese flag for the Japanese language"
+                (\config location surveyResultList ->
+                    let
+                        locale =
+                            (Locale Ja False I18Next.initialTranslations)
+
+                        model =
+                            Model
+                                config
+                                locale
+                                location
+                                ListSurveyResultsRoute
+                                NotRequested
+                                (Success surveyResultList)
+                    in
+                        model
+                            |> Main.view
+                            |> Query.fromHtml
+                            |> Query.find [ dropdownCurrentSelection ]
+                            |> Query.children [ class "flag-icon-jp" ]
+                            |> Query.count (Expect.equal 1)
+                )
+            ]
