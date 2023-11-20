7a27ff4f7c8994de24ce1243a0fdc6d0f7f82d0a
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-11-21 10:41:00 +1100

    Update back end deps

--- back_end/mix.exs
+++ back_end/mix.exs
@@ -5,7 +5,7 @@ defmodule BackEnd.Mixfile do
     [
       app: :back_end,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,11 +21,11 @@ defmodule BackEnd.Mixfile do
 
   defp deps do
     [
-      {:cors_plug, "~> 2.0"},
-      {:cowboy, "~> 2.3"},
-      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
-      {:plug_cowboy, "~> 2.0"},
-      {:jason, "~> 1.1"}
+      {:cors_plug, "~> 3.0"},
+      {:cowboy, "~> 2.10"},
+      {:mix_test_watch, "~> 1.1", only: :dev, runtime: false},
+      {:plug_cowboy, "~> 2.6"},
+      {:jason, "~> 1.4"}
     ]
   end
 end
